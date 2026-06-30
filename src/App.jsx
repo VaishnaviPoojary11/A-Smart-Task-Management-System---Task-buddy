753eb10b9de19a01724ff049af69d1d0a9f159e1
VaishnaviPoojary11 <vaishnavipoojary98@gmail.com>
2026-06-30 23:03:12 +0530

    task-buddy

---
+++ src/App.jsx
@@ -0,0 +1,48 @@
+import { useState,useEffect } from 'react';
+import TaskForm from './Components/TaskForm'
+import TaskList from './Components/TaskList'
+import ProgressTracker from './Components/ProgressTracker'
+
+
+export default function App() {
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(()=>{
+    localStorage.setItem("tasks",JSON.stringify(tasks));
+  });
+
+  const addTask = (task)=>{
+    setTasks([...tasks, task])
+  }
+
+  const updateTask = (updatedTask, index) => {
+    const newtask = [...tasks];
+    newtask[index] = updatedTask;
+    setTasks(newtask);
+  }
+  const deleteTask = (index) => {
+    setTasks(tasks.filter((_, i) => i != index ))
+  }
+
+  const clearTasks = () => {
+    setTasks([]);
+  }
+  return (
+    <div className='container'>
+      <header>
+        <h1>TaskMan</h1>
+        <p><i>Your friendly Task Manager</i></p>
+      </header>
+
+      <TaskForm addTask = {addTask}/>
+      <TaskList tasks = {tasks}
+       updateTask = {updateTask} 
+       deleteTask = {deleteTask}/>
+      <ProgressTracker />
+
+      {tasks.length>0 && (<button className='clear-btn'
+      onClick={clearTasks}>clear All Tasks</button>)}
+      
+    </div>
+  )
+}
---
+++ src/Components/ProgressTracker.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function ProgramTracker() {
+  return (
+    <div>
+      <h1>Progress Tracker</h1>
+    </div>
+  )
+}
